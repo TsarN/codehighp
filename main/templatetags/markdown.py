e1f1bd31bf2535be526f86ecca3769bcd2e38c14
Tsarev Nikita <nikts53@gmail.com>
2019-04-28 21:46:39 +0300

    Added user link tag in markdown

--- main/templatetags/markdown.py
+++ main/templatetags/markdown.py
@@ -1,10 +1,18 @@
+import re
+
 from django import template
 import markdown2
 from django.utils.safestring import mark_safe
+
+from users.models import CustomUser
 
 register = template.Library()
 
 
 @register.filter
 def markdown(value):
-    return mark_safe(markdown2.markdown(value, safe_mode=True, extras=["fenced-code-blocks"]))
+    value = markdown2.markdown(value, safe_mode=True, extras=["fenced-code-blocks"])
+    usernames = list(set(re.findall(r'\[user:([a-zA-Z0-9]+?)\]', value)))
+    for user in CustomUser.objects.filter(username__in=usernames):
+        value = value.replace('[user:%s]' % user.username, user.html_link)
+    return mark_safe(value)
