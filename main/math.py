4e0a49d49d01cab708c7c2056df22750037944fa
Tsarev Nikita <nikts53@gmail.com>
2019-05-01 22:33:27 +0300

    Multiline math

--- main/math.py
+++ main/math.py
@@ -6,14 +6,14 @@ import requests
 
 
 def replace_math(x):
-    for group in set(re.findall(r'\$\$(.*?)\$\$', x)):
+    for group in set(re.findall(r'\$\$(.*?)\$\$', x, re.MULTILINE)):
         data = requests.post(settings.MATHOID_URL, data=dict(q=group, type='tex')).json()
         if not data['success']:
             x = x.replace('$${}$$'.format(group), 'TeX error: {}'.format(data.get('log')))
         else:
             x = x.replace('$${}$$'.format(group), data.get('svg'))
 
-    for group in set(re.findall(r'\$(.*?)\$', x)):
+    for group in set(re.findall(r'\$(.*?)\$', x, re.MULTILINE)):
         data = requests.post(settings.MATHOID_URL, data=dict(q=group, type='inline-tex')).json()
         if not data['success']:
             x = x.replace('${}$'.format(group), 'TeX error: {}'.format(data.get('log')))
